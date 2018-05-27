280ab5167d2e7a28b117ef37de56db5a5b716498
kasambuli <cynthiakasambuli@gmail.com>
2018-05-27 08:07:07 +0300

    add projects models

--- Watch/Neighborhood/models.py
+++ Watch/Neighborhood/models.py
@@ -3,36 +3,41 @@ import datetime as dt
 from django.contrib.auth.models import User
 
 # Create your models here.
-class UserProfile(models.Model):
-   '''
-   class that saves the user data 
-   '''
-   user = models.OneToOneField(User, on_delete = 'models.CASCADE')
-   neighborhood = models.ForeignKey(Neighborhood, on_delete = 'models.CASCADE')
-   location = models.CharField(max_length = 30)
-
 class Neighborhood(models.Model):
     '''
     class for saving neighbourhood data
     '''
-   name  = models.CharField(max_length = 30)
+    name  = models.CharField(max_length = 30)
+    location = models.CharField(max_length = 30)
+    occupant_count = models.IntegerField()
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+   '''
+   class that saves the user data 
+   '''
+   user = models.ForeignKey(User, on_delete = models.CASCADE)
+   neighborhood = models.ForeignKey(Neighborhood, on_delete = models.CASCADE)
    location = models.CharField(max_length = 30)
-   occupant_count = models.IntegerField()
+
 
 class Business(models.Model):
     '''
     model that displays the business in an area 
     '''
-   bussiness_name = models.CharField(max_length = 30)
-   user = models.ForeignKey(user_profile, on_delete = 'models.CASCADE')
-   Neighborhood = models.ForeignKey(Neighborhood, on_delete = 'models.CASCADE')
-   Email_address = models.CharField(max_length = 30)
+    name = models.CharField(max_length = 30)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete = models.CASCADE)
+    email= models.CharField(max_length = 30)
 
 class Post(models.Model):
    '''
    model that saves posts for the neighborhood 
    '''
    title = models.CharField(max_length  = 20)
+   post = models.TextField(max_length=100)
+   avatar = models.ImageField(upload_to = 'avatar/')
    user_name = models.CharField(max_length = 30)
    time = models.DateTimeField(auto_now_add=True,blank = True)
 
@@ -43,6 +48,6 @@ class Services(models.Model):
    police_station = models.CharField(max_length = 30)
    police_no = models.IntegerField(10)
    police_location = models.CharField(max_length = 30)
-   healthcare_centre = model.CharField(max_length = 30)
+   healthcare_centre = models.CharField(max_length = 30)
    healthcare_no = models.IntegerField(10)
-   healthcare_location = models.CharField(max_length = 30 blank = True)
+   healthcare_location = models.CharField(max_length = 30 ,blank = True)
