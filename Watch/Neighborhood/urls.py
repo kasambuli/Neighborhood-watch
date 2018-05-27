1a059116b8d4c2bb5f1ced2545480d11a5c98af2
kasambuli <cynthiakasambuli@gmail.com>
2018-05-27 18:03:00 +0300

    add index and neighborhood urls

--- Watch/Neighborhood/urls.py
+++ Watch/Neighborhood/urls.py
@@ -1,8 +1,13 @@
+from django.conf.urls import url
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-#..............
+url(r'^accounts/profile/(\d+)', views.profile, name='profile'),
+url(r'^profile/', views.update_profile, name='editProfile'),
+url(r'^neighborhood/',views.neighborhood,name = 'neighborhood'),
+url(r'^$',views.index,name ='index')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
