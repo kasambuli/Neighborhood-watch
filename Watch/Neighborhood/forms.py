fa6a1722ac6a5f5ec28f69b063ea2ee6419baece
kasambuli <cynthiakasambuli@gmail.com>
2018-05-27 18:01:59 +0300

    add neighborhood form

--- Watch/Neighborhood/forms.py
+++ Watch/Neighborhood/forms.py
@@ -1,5 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Neighborhood,Business,UserProfile
 
 # profile form
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['user','name','avatar','neighborhood','location']
+
+class NeighborhoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighborhood
+        fields = ['name','location','occupant_count']
