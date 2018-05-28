0a4c1d0f9a00070fe55ac367fb960894ef676962
kasambuli <cynthiakasambuli@gmail.com>
2018-05-28 09:04:20 +0300

    add business and service form

--- Watch/Neighborhood/forms.py
+++ Watch/Neighborhood/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Neighborhood,Business,UserProfile
+from .models import Neighborhood,Business,UserProfile,Post,Services
 
 # profile form
 class UserProfileForm(forms.ModelForm):
@@ -13,3 +13,18 @@ class NeighborhoodForm(forms.ModelForm):
     class Meta:
         model = Neighborhood
         fields = ['name','location','occupant_count']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title','post','profile','user']
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        fields = ['name','user','neighborhood','email']
+
+class ServiceForm(forms.ModelForm):
+    class Meta:
+        model = Services
+        fields = ['police_station','police_no','police_location','healthcare_centre','healthcare_no','healthcare_location']
