7b5fcc1d9befdc92a1cfc46df1e4ce4047baf29b
kasambuli <cynthiakasambuli@gmail.com>
2018-05-28 09:19:53 +0300

    add business and services urls

--- Watch/Neighborhood/urls.py
+++ Watch/Neighborhood/urls.py
@@ -7,7 +7,13 @@ urlpatterns=[
 url(r'^accounts/profile/(\d+)', views.profile, name='profile'),
 url(r'^profile/', views.update_profile, name='editProfile'),
 url(r'^neighborhood/',views.neighborhood,name = 'neighborhood'),
-url(r'^$',views.index,name ='index')
+url(r'^$',views.index,name ='index'),
+url(r'^post/',views.post,name = 'post'),
+url(r'^viewpost/',views.viewPost,name = 'viewpost'),
+url(r'^business/',views.business,name = 'business'),
+url(r'^viewbusiness/',views.viewBusiness,name = 'viewbusiness'),
+url(r'^services/',views.services,name = 'services'),
+url(r'^viewservices/',views.viewServices,name = 'viewservices'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
