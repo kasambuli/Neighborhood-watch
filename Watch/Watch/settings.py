928c7eddb6b8d3ea5b3181696051c3d1f4de3350
kasambuli <cynthiakasambuli@gmail.com>
2018-05-28 09:23:31 +0300

    add bootstrap 4

--- Watch/Watch/settings.py
+++ Watch/Watch/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Neighborhood.apps.NeighborhoodConfig',
-    'bootstrap3',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
